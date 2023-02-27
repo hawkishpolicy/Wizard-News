e75d9f80a330bf1440d575c48ec1df78f2856424
Thomas <thomasjohnso@gmail.com>
2023-02-27 14:45:51 -0500

    added access to public folder, edited html to style site

--- app.js
+++ app.js
@@ -5,21 +5,52 @@ const postBank = require("./postBank");
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   const posts = postBank.list();
 
-  const html = `<!DOCTYPE html>
-<html>
-<head>
-  <title>Wizard News</title>
-</head>
-<body>
-<ul>
-  ${posts.map((post) => `<li>${post.title}</li>,<p>${post.content}</p>`)}
-</ul>
-</body>
+  const html = `<DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts
+        .map(
+          (post) => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+        )
+        .join("")}
+    </div>
+  </body>
 </html>`;
+
+  //   const html = `<!DOCTYPE html>
+  // // <html>
+  // // <head>
+  // //   <title>Wizard News</title>
+  // // </head>
+  // // <body>
+  // // <ul>
+  // //   ${posts
+  //     .map((post) => `<li>${post.title}</li><p>${post.content}</p>`)
+  //     .join("")}
+  // </ul>
+  // </body>
+  // </html>`;
   res.send(html);
 });
 
